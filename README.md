4590690556bef67097319d77ffa7961928da3e72
李婷 <liting@wlmkids.com>
2021-10-13 16:47:14 +0800

    feat: eslint 自定义插件,max-params

--- README.md
+++ README.md
@@ -40,8 +40,4 @@ Then configure the rules you want to use under the rules section.
 }
 ```
 
-## Supported Rules
-
-* Fill in provided rules here
-
 
---
+++ tests/lib/rules/max-params.js
@@ -0,0 +1,14 @@
+const { RuleTester,rule } = require("eslint");
+
+var ruleTester = new RuleTester();
+ruleTester.run('max-params', rule, {
+  valid: ['function test(d,e,f) {}'],
+  invalid: [
+    {
+      code: 'function test(a,b,c,d) {}',
+      errors: [{
+        message: '参数最多不能超过3个',
+      }]
+    }
+  ]
+})
